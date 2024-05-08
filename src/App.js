45c4eaafbbf68a7eee5e9c08d33a699710d5a562
tim <4b0g0037@stust.edu.tw>
2024-05-08 23:10:13 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>學號:4B0G0037  姓名:林定燁</p>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+        </Slider>
+      </header>
+    </div>
+  );
+}
+
+export default App;
