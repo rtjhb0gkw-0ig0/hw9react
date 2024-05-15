9e9677f154837506b3567a635092d33c7455e919
tim <4b0g0037@stust.edu.tw>
2024-05-15 22:30:31 +0800

    hw6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MyRGBPanel from './MyRGBPanel';
+
+import React from 'react';
+import Slider from './MySlider';
 
 function App() {
+ const handleSliderChange = (value) => {
+    console.log('Slider Value:', value);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <p>學號: 4B0G0037  姓名: 林定燁</p>
-        <MyRGBPanel />
+        <p>學號:4B0G0037  姓名:林定燁</p>
+        <Slider
+        min={0}
+        max={100}         
+        step={1}              
+        initialValue={50}    
+        onChange={handleSliderChange}
+		/>
       </header>
     </div>
   );
